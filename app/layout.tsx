d77dd94d3619cef21089d5afe4b0fb4cd510ae85
pragatipatil1428 <pragatipatil1428@gmail.com>
2026-07-27 11:17:38 +0530

    bug fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      data-scroll-behavior="smooth"
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
       <body className="min-h-screen bg-background text-foreground antialiased">
